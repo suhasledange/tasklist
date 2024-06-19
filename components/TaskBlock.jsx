37b4dec22169924bd313e05095ae42a77dc25797
suhasledange <suhasledange10@gmail.com>
2024-06-19 23:34:16 +0530

    ui

--- components/TaskBlock.jsx
+++ components/TaskBlock.jsx
@@ -31,10 +31,10 @@ const TaskBlock = () => {
                 initialTask={initialTask}
                 />
     <div className='flex flex-col max-w-7xl w-full mx-auto shadow-slate-400 shadow-md my-8 '>
-        <div className='flex md:flex-row flex-col md:items-center md:gap-0 gap-5 justify-between px-6 py-5 bg-gray-200/[0.5]'>
+        <div className='flex md:flex-row flex-col md:items-center md:gap-0 gap-5 justify-between px-4 py-5 bg-gray-200/[0.5]'>
             <div className='flex md:gap-8 gap-12  items-center'>
                 <h1 className='font-bold text-sm md:text-md tracking-wider uppercase'>Sales Log</h1>
-                <button onClick={handleAddTask} className='active:scale-95 border-2 md:text-md text-sm border-black/[0.5] rounded-sm py-2 px-4 md:px-6 gap-3 flex items-center justify-center'>
+                <button onClick={handleAddTask} className='active:scale-95 border-2 md:text-md text-sm border-black/[0.5] rounded-sm py-2 px-4 gap-3 flex items-center justify-center'>
                     <TbCirclePlus className='text-xl  text-blue' />
                     New Task
                 </button>
